c306fc5cd90e417ca611acb79989e7930655cff0
MateuszPawelczyk <mateusz.pawelczyk.ext@murrelektronik.com>
2023-04-28 18:53:03 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/ChessAI_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessAI_Project.dir/main.cpp.obj"
+  "CMakeFiles/ChessAI_Project.dir/main.cpp.obj.d"
+  "ChessAI_Project.exe"
+  "ChessAI_Project.exe.manifest"
+  "ChessAI_Project.lib"
+  "ChessAI_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessAI_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Beste KI der Welt!" << std::endl;
+    std::cout << "Beste KI der Welt! Mati und Niklas sind cool" << std::endl;
     return 0;
 }
