7cc7803aef8cff74aa9ba4da8c21d29cd81f029c
MateuszPawelczyk <mateusz.pawelczyk.ext@murrelektronik.com>
2023-05-02 18:28:10 +0200

    fixed tests

--- components/pieces/bishop.cpp
+++ components/pieces/bishop.cpp
@@ -20,12 +20,13 @@ std::array<uint8_t, MAX_MOVES> bishop::possibleMoves(uint64_t interferedBoard, u
 
     uint8_t moveCount = 0;
     // diagonal north east
-    for (uint8_t i = 1; i < 8 && newX < 8 && newY < 8 && !(ownColorBoard & (1ULL << (newY * 8 + newX))); i++)
+    for (uint8_t i = 1; i < 8 && newX < 8 && newY < 8 && !(ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))); i++)
     {
         possibleMoves[moveCount] = ((newX << 3) | newY);
-        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + newX))) {
+        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))) {
             // Set the 8th bit to 1 to indicate that the move is a capture move.
             possibleMoves[moveCount] |= 0b10000000;
+            moveCount++;
             break;
         }
         newX++;
@@ -37,12 +38,13 @@ std::array<uint8_t, MAX_MOVES> bishop::possibleMoves(uint64_t interferedBoard, u
     newX = x + 1;
     newY = y - 1;
     // diagonal south east
-    for (uint8_t i = 1; i < 8 && newX < 8 && newY >= 0 && !(ownColorBoard & (1ULL << (newY * 8 + newX))); i++)
+    for (uint8_t i = 1; i < 8 && newX < 8 && newY >= 0 && !(ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))); i++)
     {
         possibleMoves[moveCount] = (newX << 3) | newY;
-        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + newX))) {
+        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))) {
             possibleMoves[moveCount] |= 0b10000000;
             // Set the 8th bit to 1 to indicate that the move is a capture move.
+            moveCount++;
             break;
         }
         newX++;
@@ -53,12 +55,13 @@ std::array<uint8_t, MAX_MOVES> bishop::possibleMoves(uint64_t interferedBoard, u
     newX = x - 1;
     newY = y - 1;
     // diagonal south west
-    for (uint8_t i = 1; i < 8 && newX >= 0 && newY >= 0 && !(ownColorBoard & (1ULL << (newY * 8 + newX))); i++)
+    for (uint8_t i = 1; i < 8 && newX >= 0 && newY >= 0 && !(ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))); i++)
     {
         possibleMoves[moveCount] = (newX << 3) | newY;
-        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + newX))) {
+        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))) {
             // Set the 8th bit to 1 to indicate that the move is a capture move.
             possibleMoves[moveCount] |= 0b10000000;
+            moveCount++;
             break;
         }
         newX--;
@@ -69,12 +72,13 @@ std::array<uint8_t, MAX_MOVES> bishop::possibleMoves(uint64_t interferedBoard, u
     newX = x - 1;
     newY = y + 1;
     // diagonal north west
-    for (uint8_t i = 1; i < 8 && newX >= 0 && newY < 8 && !(ownColorBoard & (1ULL << (newY * 8 + newX))); i++)
+    for (uint8_t i = 1; i < 8 && newX >= 0 && newY < 8 && !(ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))); i++)
     {
         possibleMoves[moveCount] = (newX << 3) | newY;
-        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + newX))) {
+        if (interferedBoard & ~ownColorBoard & (1ULL << (newY * 8 + (7 - newX)))) {
             // Set the 8th bit to 1 to indicate that the move is a capture move.
             possibleMoves[moveCount] |= 0b10000000;
+            moveCount++;
             break;
         }
         newX--;
@@ -82,6 +86,9 @@ std::array<uint8_t, MAX_MOVES> bishop::possibleMoves(uint64_t interferedBoard, u
         moveCount++;
     }
 
+    if (moveCount < MAX_MOVES - 1) {
+        possibleMoves[moveCount] = 0b01000000;
+    }
 
     return possibleMoves;
 }
--- components_tests/Pieces_Tests.cpp
+++ components_tests/Pieces_Tests.cpp
@@ -20,10 +20,43 @@ TEST(PiecesTest, PieceTest1) {
                                 ,0b00011011,0b00010010,0b00001001,0b00000000
                                 ,0b00011101,0b00010110,0b00001111};
 
-    std::array<uint8_t, 13> y = bishop::possibleMoves(board.GetAllPieces(),board.GetWhitePieces(),board.GetPosition("e5"));
+    std::array<uint8_t, 13> y = bishop::possibleMoves(board.GetAllPieces(),board.GetBlackPieces(),board.GetPosition("e5"));
 
     for (int i = 0; i < 13; ++i) {
         EXPECT_EQ(expectedMoves[i], y[i]);
     }
 }
 
+TEST(PiecesTest, PieceTest2) {
+    BitBoardLoader loader = BitBoardLoader("./bitboards.txt");
+
+    Board board("8/2r5/8/4b3/8/8/8/8");
+    std::cout << board.ToString(loader);
+
+    uint8_t expectedMoves[13] = {0b00101101,0b00110110,0b00111111
+                                ,0b00101011,0b00110010,0b00111001
+                                ,0b00011011,0b00010010,0b00001001,0b00000000
+                                ,0b00011101,0b01000000};
+
+    std::array<uint8_t, 13> y = bishop::possibleMoves(board.GetAllPieces(),board.GetBlackPieces(),board.GetPosition("e5"));
+
+    for (int i = 0; i < 13 && expectedMoves[i] != 0b01000000; ++i) {
+        EXPECT_EQ(expectedMoves[i], y[i])<<"differs at index"<<i;
+    }
+}
+
+TEST(PiecesTest, PieceTest3) {
+    std::filesystem::path path = "./bitboards.txt";
+
+    BitBoardLoader loader = BitBoardLoader("./bitboards.txt");
+
+    Board board("8/8/8/8/3Q4/8/1b6/8 w - - 0 1");
+    std::cout << board.ToString(loader);
+    uint8_t expectedMoves[13] = {0b00010010, 0b10011011, 0b00010000, 0b00000000, 0b00000010, 0b01000000};
+
+    std::array<uint8_t, 13> y = bishop::possibleMoves(board.GetAllPieces(),board.GetBlackPieces(),board.GetPosition("b2"));
+
+    for (int i = 0; i < 13 && expectedMoves[i] != 0b01000000; ++i) {
+        EXPECT_EQ(expectedMoves[i], y[i]) << "differs at index "<< i;
+    }
+}
