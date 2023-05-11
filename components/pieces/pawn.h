2cfb72a03916437a5a9b283edb747ac0eaf9d250
MateuszPawelczyk <mateusz.pawelczyk.ext@murrelektronik.com>
2023-05-11 11:33:49 +0200

    changed to MOVE_ARRAY

--- components/pieces/pawn.h
+++ components/pieces/pawn.h
@@ -8,7 +8,7 @@ class pawn
 {
 public:
 
-    static void possibleMoves(std::vector<MOVE> &moves, BOARD allPieces, BOARD currentColor, uint8_t x, uint8_t y, bool color, uint8_t en_passante);
+    static void possibleMoves(MOVE_ARRAY &moves, BOARD allPieces, BOARD currentColor, uint8_t x, uint8_t y, bool color, uint8_t en_passante);
 
 };
 
